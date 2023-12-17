561afa290f39089de4a8dc41abe69a5ee7845d7a
samy <123843947+samydev65@users.noreply.github.com>
2023-12-17 01:59:47 -0500

    Delete Mom directory

--- Mom/navactive.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".nav-link").forEach((link) => {
-  if (link.href === window.location.href) {
-      link.classList.add("active");
-      link.setAttribute("aria-current", "page");
-  }
-});
